c582aa474dad63c35299f4cfebc737f3c801fadd
Francois C. M. Berenger <francois.berenger@nxera.life>
2024-10-30 12:40:25 +0000

    bin/acp4_get_pocket.py: support output file being a .sdf
    in addition to the default: being a .pdb

--- bin/acp4_get_pocket.py
+++ bin/acp4_get_pocket.py
@@ -6,7 +6,7 @@
 # ^CHAIN:char<SPACE>RESNUM:int$ lines
 
 import os, sys
-from prody import *
+from prody import parsePDB, writePDBStream
 
 def load_pocket_definition(pocket_fn):
     res = []
@@ -15,12 +15,12 @@ def load_pocket_definition(pocket_fn):
         split = line.split()
         chain = str(split[0])
         if len(chain) != 1:
-            print("FATAL: get_pocket.py: load_pocket_definition: strange chain: %s" % line,
+            print("FATAL: acp4_get_pocket.py: load_pocket_definition: strange chain: %s" % line,
                   file=sys.stderr)
             exit(1)
         resnum = int(split[1])
         if resnum <= 0:
-            print("FATAL: get_pocket.py: load_pocket_definition: strange resnum: %s" % line,
+            print("FATAL: acp4_get_pocket.py: load_pocket_definition: strange resnum: %s" % line,
                   file=sys.stderr)
             exit(1)
         chain_res = (chain, resnum)
@@ -39,7 +39,23 @@ def group_by_chain(residues):
 #      or, at least usage message in case not enough params
 pdb_fn = sys.argv[1]
 pocket_fn = sys.argv[2]
-output_fn = sys.argv[3]
+output_fn = sys.argv[3] # this one is either a .pdb file (default) or a .sdf
+                        # in which case obabel will be called to do pdb2sdf
+
+pdb_output_fn = ''
+sdf_output_fn = ''
+
+convert_pdb_to_sdf = False
+
+if output_fn.endswith('.sdf'):
+    convert_pdb_to_sdf = True
+    pdb_output_fn = output_fn.replace('.sdf', '.pdb')
+    sdf_output_fn = output_fn
+
+if output_fn.endswith('.pdb'):
+    convert_pdb_to_sdf = False
+    pdb_output_fn = output_fn
+    sdf_output_fn = ''
 
 try:
     pdb = parsePDB(pdb_fn)
@@ -49,7 +65,7 @@ except:
 else:
     pocket_residues = load_pocket_definition(pocket_fn)
     chain_residues = group_by_chain(pocket_residues)
-    with open(output_fn, 'w') as output:
+    with open(pdb_output_fn, 'w') as output:
         for chain, residues in chain_residues.items():
             resnums = list(map(str, residues))
             resnums_str = ' '.join(resnums)
@@ -57,3 +73,9 @@ else:
             atoms = pdb.select(selection)
             if atoms:
                 writePDBStream(output, atoms)
+    if sdf_output_fn != '':
+        # try calling obabel for pdb2sdf conversion
+        cmd = 'obabel %s -O %s' % (pdb_output_fn, sdf_output_fn)
+        print("INFO: acp4_get_pocket.py: running: %s" % cmd,
+              file=sys.stderr)
+        os.system(cmd)
