133cdb0ed02ccc3fc0d14a531ef853c333c10c9d
Francois Berenger <ligand@free.fr>
2025-01-15 14:54:12 +0000

    Superpose: specification

--- src/superpose.ml
+++ src/superpose.ml
@@ -1,4 +1,14 @@
 
 let main () =
+  (*
+  given two .ph4 files, assume:
+  - LHS = A: the reference structure
+  - RHS = B: the candidate structure
+  - find the best rotation and translation of B that minimizes error(A, B).
+  Each ph4 type in A is BST indexed first.
+  To evaluate one rotation and one translation, the error function is:
+  - for each ph4 point in B, add the distance to its nearest neighbor in A
+    which has the same type into the error sum
+  At the end, output the (rot, trans) w/ the smallest error.
+  *)
   failwith "not implemented yet"
-    
