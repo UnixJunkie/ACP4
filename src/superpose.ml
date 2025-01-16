ccc7619b69594d380db3c5043eec98829c8cee2c
Francois Berenger <ligand@free.fr>
2025-01-16 16:53:05 +0000

    Superpose: starting to work
    discrete rotational search followed by global optimization
    w/ a smart initial solution seems to work great;
    at least to superpose a molecule w/ a randomly posed
    copy of itself

--- src/superpose.ml
+++ src/superpose.ml
@@ -5,11 +5,12 @@ module Log = Dolog.Log
 module LO = Line_oriented
 module V3 = Mmo.V3
 module Rot = Mmo.Rot
+module SO3 = Mmo.SO3
 
 open Printf
 
 (* FBR: TODO: turn on clustering of features (Ph4.cluster_features)
-   via a CLI option *)
+   via a CLI option? *)
 
 let read_one_ph4_molecule fn =
   LO.with_in_file fn Ph4.read_one_ph4_encoded_molecule
@@ -42,6 +43,9 @@ let geometric_center vects =
     (A.fold_left V3.add V3.origin vects)
     (float (A.length vects))
 
+let center_of m =
+  geometric_center Ph4.(m.coords)
+
 let translate_by vects delta =
   let dx, dy, dz = V3.to_triplet delta in
   A.map (fun V3.{x; y; z} ->
@@ -54,9 +58,15 @@ let translate_to vects p =
 let center_molecule m =
   Ph4.{ m with coords = translate_to m.coords V3.origin }
 
+let translate_molecule m p =
+  Ph4.{ m with coords = translate_to m.coords p }
+
 (* CONSTRAINT: vects must already be centered at origin *)
 let centered_rotate vects r =
   A.map (Rot.rotate r) vects
+
+let rotate_centered_molecule m r =
+  Ph4.{ m with coords = centered_rotate m.coords r }
 
 (* rotate then translate already centered molecule [centered_m] *)
 let place_molecule centered_m params =
@@ -69,54 +79,59 @@ let place_molecule centered_m params =
           translate_to rotated (V3.make params.(3) params.(4) params.(5)) }
 
 (* we don't have a gradient --> _gradient *)
-let nlopt_eval_solution verbose bsts centered_m params _gradient =
+let nlopt_eval_solution bsts centered_m params _gradient =
   let posed = place_molecule centered_m params in
   let error = score_molecule_pose bsts posed in
-  (if verbose then
-     Log.info "err: %f" error
-  );
+  Log.debug "err: %f" error;
   error
 
+let pi = Mmo.Math.pi
 let two_pi = Mmo.Math.two_pi
 
-let nlopt_optimize verbose bsts centered_m max_evals =
+let nlopt_optimize init_params bsts centered_m max_evals =
   let ndims = 6 in (* DOFs: 3 rotations + 3 translations *)
   (* local optimizer that will be passed to the global one *)
   let local = Nlopt.(create sbplx ndims) in (* local optimizer: gradient-free *)
-  Nlopt.set_min_objective local (nlopt_eval_solution verbose bsts centered_m);
+  Nlopt.set_min_objective local (nlopt_eval_solution bsts centered_m);
   (* I don't set parameter bounds on the local optimizer, I guess
    * the global optimizer handles this *)
   (* hard stop conditions *)
   Nlopt.set_stopval local 0.0; (* absolute minimum total error *)
-  (* smart stop conditions *)
-  Nlopt.set_ftol_abs local 0.0001; (* might need to be tweaked *)
+  (* (\* smart stop conditions *\) *)
+  (* Nlopt.set_ftol_abs local 0.0001; (\* might need to be tweaked *\) *)
   let global = Nlopt.(create auglag ndims) in (* global optimizer *)
   Nlopt.set_local_optimizer global local;
-  Nlopt.set_min_objective global (nlopt_eval_solution verbose bsts centered_m);
+  Nlopt.set_min_objective global (nlopt_eval_solution bsts centered_m);
   (* bounds for parameters *)
   (* FBR: found tx_min, ty_min, tz_min using bounding boxes? *)
-  (*                              rz      ry      rz       tx       ty       tz *)
-  Nlopt.set_lower_bounds global [|0.0;    0.0;    0.0;    -1000.0; -1000.0; -1000.0|];
-  Nlopt.set_upper_bounds global [|two_pi; two_pi; two_pi;  1000.0;  1000.0;  1000.0|];
+  (*                              rz    ry    rz    tx       ty       tz *)
+  Nlopt.set_lower_bounds global [|-.pi; -.pi; -.pi; -1000.0; -1000.0; -1000.0|];
+  Nlopt.set_upper_bounds global [|  pi;   pi;   pi;  1000.0;  1000.0;  1000.0|];
   (* hard stop conditions *)
   Nlopt.set_stopval global 0.0;
   (* max number of iterations *)
   Nlopt.set_maxeval global max_evals;
-  (* starting solution *)
-  let start_sol = Array.make ndims 0.0 in (* no rot., no trans. *)
-  let stop_cond, params, error = Nlopt.optimize global start_sol in
+  let stop_cond, params, error = Nlopt.optimize global init_params in
   Log.info "NLopt optimize global: %s" (Nlopt.string_of_result stop_cond);
-  Log.info "final error: %f" error;
-  params
+  Log.info "error: %f" error;
+  (params, error)
 
 let random_start m =
   Random.self_init ();
-  place_molecule m [|Random.float two_pi;
-                     Random.float two_pi;
-                     Random.float two_pi;
+  place_molecule m [|-.pi +. Random.float two_pi;
+                     -.pi +. Random.float two_pi;
+                     -.pi +. Random.float two_pi;
                      -1000.0 +. Random.float 2000.;
                      -1000.0 +. Random.float 2000.;
                      -1000.0 +. Random.float 2000.|]
+
+let bsts_for_molecule m =
+  [|BST.(create 1 Two_bands) (Ph4.get_coords_with_feat m ARO);
+    BST.(create 1 Two_bands) (Ph4.get_coords_with_feat m HYD);
+    BST.(create 1 Two_bands) (Ph4.get_coords_with_feat m HBA);
+    BST.(create 1 Two_bands) (Ph4.get_coords_with_feat m HBD);
+    BST.(create 1 Two_bands) (Ph4.get_coords_with_feat m POS);
+    BST.(create 1 Two_bands) (Ph4.get_coords_with_feat m NEG)|]
 
 let main () =
   Log.(set_prefix_builder short_prefix_builder);
@@ -147,35 +162,63 @@ let main () =
   let ref_mol = read_one_ph4_molecule ref_fn in
   let cand_mol =
     center_molecule
-      (if rand_start then
-         random_start (read_one_ph4_molecule cand_fn)         
+      (let mol = read_one_ph4_molecule cand_fn in
+       if rand_start then
+         random_start mol
        else
-         read_one_ph4_molecule cand_fn) in
+         mol) in
+  (* ROTATIONAL SEARCH ONLY ------------------------------------------------ *)
+  Log.info "discrete rotational search...";
+  (* center both molecules, then brute force search over a discretized
+     space of all possible rotations *)
+  let centered_ref_mol = center_molecule ref_mol in
+  let centered_ref_mol_bsts = bsts_for_molecule centered_ref_mol in
+  (* at the end, translate the best rotated candidate there *)
+  let ref_mol_init_center = center_of ref_mol in
+  (* brute force search of the best rotation *)
+  let rotations = SO3.rotations 100_000 in
+  let best_score = ref infinity in
+  let best_rot = ref (Rot.id ()) in
+  A.iter (fun rot ->
+      let rotated = rotate_centered_molecule cand_mol rot in
+      let score = score_molecule_pose centered_ref_mol_bsts rotated in
+      if score < !best_score then
+        (Log.debug "score: %f" score;
+         best_score := score;
+         best_rot := rot)
+    ) rotations;
+  Log.info "error: %f" !best_score;
+  (* rotate centered cand. *)
+  let rotated = rotate_centered_molecule cand_mol !best_rot in
+  (* translate it back *)
+  let translated = translate_molecule rotated ref_mol_init_center in
+  Log.info "writing: %s" output_fn;
+  Ph4.to_file output_fn translated;
+  (* try global optim. of this initial solution
+     if it improves the current solution, we will report it *)
+  (* GLOBAL SEARCH --------------------------------------------------------------- *)
+  Log.info "global optimization...";
   (* Each ph4 type in A is BST indexed first *)
-  let aro_coords = Ph4.get_coords_with_feat ref_mol ARO in
-  let hyd_coords = Ph4.get_coords_with_feat ref_mol HYD in
-  let hba_coords = Ph4.get_coords_with_feat ref_mol HBA in
-  let hbd_coords = Ph4.get_coords_with_feat ref_mol HBD in
-  let pos_coords = Ph4.get_coords_with_feat ref_mol POS in
-  let neg_coords = Ph4.get_coords_with_feat ref_mol NEG in
-  let aro_bst = BST.(create 1 Two_bands) aro_coords in
-  let hyd_bst = BST.(create 1 Two_bands) hyd_coords in
-  let hba_bst = BST.(create 1 Two_bands) hba_coords in
-  let hbd_bst = BST.(create 1 Two_bands) hbd_coords in
-  let pos_bst = BST.(create 1 Two_bands) pos_coords in
-  let neg_bst = BST.(create 1 Two_bands) neg_coords in
-  let bsts = [|aro_bst; hyd_bst; hba_bst; hbd_bst; pos_bst; neg_bst|] in
+  let ref_mol_bsts = bsts_for_molecule ref_mol in
   (* - find the best rotation and translation of B that minimizes error(A, B). *)
   (* At the end, output the (rot, trans) w/ the smallest error. *)
-  let best_params = nlopt_optimize verbose bsts cand_mol 1000 in
-  Log.info "best_params: %f %f %f %f %f %f"
-    best_params.(0)
-    best_params.(1)
-    best_params.(2)
-    best_params.(3)
-    best_params.(4)
-    best_params.(5);
-  let cand_opt = place_molecule cand_mol best_params in
-  Ph4.to_file output_fn cand_opt
+  let rx, ry, rz = Rot.decompose !best_rot in
+  let cx, cy, cz = V3.to_triplet ref_mol_init_center in
+  (* start solution: best rotation found so far; translated at ref_mol's center *)
+  let init_params = [|rx; ry; rz; cx; cy; cz|] in
+  let best_params, error = nlopt_optimize init_params ref_mol_bsts cand_mol 100_000 in
+  if error < !best_score then
+    (Log.info "score improved: %f" error;
+     Log.info "best_params: %f %f %f %f %f %f"
+       best_params.(0)
+       best_params.(1)
+       best_params.(2)
+       best_params.(3)
+       best_params.(4)
+       best_params.(5);
+     let cand_opt = place_molecule cand_mol best_params in
+     Log.info "writing: %s" output_fn;
+     Ph4.to_file output_fn cand_opt
+    )
 
 let () = main ()
