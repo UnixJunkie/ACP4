53b6cea7d433c9f87688e24cb31e44c10c3127b1
Francois Berenger <ligand@free.fr>
2025-01-20 13:59:32 +0000

    bin/acp4_pdb_move.py: new CLI options
    --relative, -ip

--- bin/acp4_pdb_move.py
+++ bin/acp4_pdb_move.py
@@ -24,27 +24,42 @@ def rot_from_rx_ry_rz(rx: float, ry: float, rz: float):
     r = np.matmul(np.matmul(rz, ry), rx)
     return Transformation(r, no_trans)
 
-def read_three_floats(line: str):
-    if line == None:
-        return None
+def read_last_six_params(fn):
+    lines = open(fn).readlines()
+    lines = list(map(lambda s: s.strip(), lines))
+    n = len(lines)
+    if n == 6:
+        return (float(lines[0]),
+                float(lines[1]),
+                float(lines[2]),
+                float(lines[3]),
+                float(lines[4]),
+                float(lines[5]))
+    elif n == 12:
+        return (float(lines[6]),
+                float(lines[7]),
+                float(lines[8]),
+                float(lines[9]),
+                float(lines[10]),
+                float(lines[11]))
     else:
-        x, y, z = line.strip().split(',')
-        return (float(x), float(y), float(z))
+        print('acp4_pdb_move.py: read_last_six_params: file %s has %d lines' %
+              (fn, n), file=sys.stderr)
+        exit(1)
 
 if __name__ == "__main__":
     # CLI options parsing -----------------------------------------------------
     parser = argparse.ArgumentParser(
         description = "rotate then translate a pdb file")
     parser.add_argument("-i", metavar = "input.pdb", dest = "input_fn",
-                        help = "input file")
+                        help = "input PDB to move")
+    parser.add_argument("-ip", metavar = "input.txt", dest = "in_params_fn",
+                        help = "optimal parameters input file")
     parser.add_argument("-o", metavar = "output.pdb", dest = "output_fn",
-                        help = "output file")
-    parser.add_argument("-r", metavar = "rx,ry,rz", dest = "r_xyz",
-                        help = "angular rotations", default='0.,0.,0.')
-    parser.add_argument("-by", metavar = "tx,ty,tz", dest = "by_xyz",
-                        help = "relative translation", default=None)
-    parser.add_argument("-to", metavar = "tx,ty,tz", dest = "to_xyz",
-                        help = "absolute translation", default=None)
+                        help = "moved PDB output")
+    parser.add_argument('--relative', dest='relative', action='store_true',
+                        help = "relative translational move (default: absolute)",
+                        default = False)
     if len(sys.argv) == 1:
         # show help in case user has no clue of what to do
         parser.print_help(sys.stderr)
@@ -52,20 +67,18 @@ if __name__ == "__main__":
     args = parser.parse_args()
     # -------------------------------------------------------------------------
     input_fn: str = args.input_fn
+    in_params_fn: str = args.in_params_fn
     output_fn: str = args.output_fn
-    rx, ry, rz = read_three_floats(args.r_xyz)
-    by_xyz = read_three_floats(args.by_xyz)
-    to_xyz = read_three_floats(args.to_xyz)
+    relative = args.relative
+    rx, ry, rz, dx, dy, dz = read_last_six_params(in_params_fn)
     # 1) center PDB
     atoms = parsePDB(input_fn)
     previous_center = calcCenter(atoms)
     print('prev_center: %s' % previous_center)
     new_center = None
-    if by_xyz != None:
-        dx, dy, dz = by_xyz
+    if relative:
         new_center = previous_center + np.array([dx, dy, dz])
-    if to_xyz != None:
-        dx, dy, dz = to_xyz
+    else: # absolute translation
         new_center = np.array([dx, dy, dz])
     print('next_center: %s' % new_center)
     moveAtoms(atoms, to=origin)
