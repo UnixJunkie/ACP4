7d40f858e6b954a8e518116f481b217992ef4d95
Francois Berenger <ligand@free.fr>
2025-01-20 13:39:01 +0000

    bin/acp4_pdb_superpose.sh: WIP

--- bin/acp4_pdb_superpose.sh
+++ bin/acp4_pdb_superpose.sh
@@ -12,15 +12,15 @@ REF_PDB=$1
 MOV_PDB=$2
 
 DIR=`dirname ${MOV_PDB}`
-BASE=`basename ${MOV_PDB}`
+BASE=`basename ${MOV_PDB} .pdb`
 # output file: superposed PDB
 SUP_PDB=${DIR}/${BASE}_sup.pdb
 
 echo "WARNING: heuristic method" >> /dev/stderr
 
 TMP_OUT_DIR=`mktemp -d`
-# echo ${TMP_OUT_DIR} # DEBUG
-trap 'rm -rf ${TMP_OUT_DIR}' EXIT
+echo "DEBUG: "${TMP_OUT_DIR}
+# trap 'rm -rf ${TMP_OUT_DIR}' EXIT
 
 # get ref and curr in ph4 format
 obabel ${REF_PDB} -O ${TMP_OUT_DIR}/ref.sdf 2>&1 | tee ${TMP_OUT_DIR}/obabel.log
@@ -35,13 +35,14 @@ export LD_LIBRARY_PATH=/apps/nlopt/2.7.1/lib64:${LD_LIBRARY_PATH}
 acp4_superpose -ref ${TMP_OUT_DIR}/ref.ph4 -cand ${TMP_OUT_DIR}/mov.ph4 \
                -o ${TMP_OUT_DIR}/sup.ph4 2>&1 | tee ${TMP_OUT_DIR}/acp4_superpose.log
 
-# apply it
-# WARNING: prefix each float w/ a space
-# so that negative numbers are not considered CLI options
-# FBR: get transform params from last line in prev. command
-acp4_pdb_move.py -i ${MOV_PDB} -o opt.pdb \
-                 -r " -1.525810, -0.559073, 0.977297" \
-                 -to " 7.737021, -33.434615, -32.893111"
+PARAMS=`grep -A1 -F "best_params:" ${TMP_OUT_DIR}/acp4_superpose.log | tail -1`
+echo "DEBUG: "${PARAMS}
+
+# FBR: pass params in a 6 lines file
+echo "INFO: optimally superposed pdb in: "${SUP_PDB}
+acp4_pdb_move.py -i ${MOV_PDB} -o ${SUP_PDB} \
+                 `echo ${PARAMS} | \
+                 awk "{printf("-r \' %f, %f, %f\' -to \' %f, %f, %f\'",$1,$2,$3,$4,$5,$6)}"`
 
 # # visualize if DEBUG
 # pymol data/3rfm_xtal_lig.pdb opt.pdb
